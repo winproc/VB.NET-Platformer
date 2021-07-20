cadad151565dd7f5997ee3f71a32dcd0d6ee3ff4
noname000002 <73682089+noname000002@users.noreply.github.com>
2021-07-20 10:11:38 +0530

    updated code

--- formg.vb
+++ formg.vb
@@ -1,1 +1,45 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
 
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+
+    Private components As System.ComponentModel.IContainer
+
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.PictureBox()
+        CType(Me.Label1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+
+        Me.Label1.BackColor = System.Drawing.Color.DarkRed
+        Me.Label1.Location = New System.Drawing.Point(374, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 27)
+        Me.Label1.TabIndex = 1
+        Me.Label1.TabStop = False
+
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Platformer"
+        CType(Me.Label1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As PictureBox
+End Class
